c387f6698e61ac3573ce9ac30c1baaa929f027a7
Roy Tsabari <a300600@gmail.com>
2012-12-25 17:51:44 +0200

    some vimrc changes and autocomplete plugin

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
-cp ~/dotfiles/bash/.bashrc ~/
-cp ~/dotfiles/git/.gitconfig ~/
+sudo cp ~/dotfiles/bash/.bashrc ~/
+sudo cp ~/dotfiles/git/.gitconfig ~/
+sudo cp ~/dotfiles/vim/.vimrc ~/
+sudo cp -R ~/dotfiles/vim/.vim ~/
+
