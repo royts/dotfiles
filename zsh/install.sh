85c05581dcd2d63ed2fa01bb627a253c5db47c38
Roy Tsabari <a300600@gmail.com>
2016-04-10 22:43:29 +0300

    .zshrc

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,3 +2,4 @@ USER_HOME=~/
 SCRIPT_LOCATION="$(cd "$(dirname "$0")" && pwd)"
 
 ln -s $SCRIPT_LOCATION/royts.zsh-theme $USER_HOME/.oh-my-zsh/themes/royts.zsh-theme
+ln -s $SCRIPT_LOCATION/.zshrc $USER_HOME/.zshrc
